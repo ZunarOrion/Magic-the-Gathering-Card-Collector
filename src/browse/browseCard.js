5f6a493f8ef7db9d4f290d025c7d49fc535db5bc
Rasmus-Strångert <Rasmus_strangert@hotmail.com>
2025-09-09 12:17:55 +0200

    fix: Trying to get the first merge to work.

--- src/browse/browseCard.js
+++ src/browse/browseCard.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.renderBrowseCardPage = renderBrowseCardPage;
-function renderBrowseCardPage() {
-    return document.querySelector("#pageContent").innerHTML = "\n    <header> \n        <!--Button which sends the user back to main page.-->\n        <button type=\"button\" class=\"logoBtn\">Logo!</button>\n\n        <!--A searchbar that will show relavant responses to what is in the input field.-->\n        <input type=\"text\" placeholder=\"Search Card...\" class=\"searchBar\"></input>\n\n        <!--A button that sends the user to another html page for advanced searches.-->\n        <button type=\"button\" class=\"advancedSearchBtn\">Advanced Search</button>\n\n        <!--A button that sends the user to another html page for logging in.-->\n        <button type=\"button\" class=\"logInBtn\">Login</button>\n    </header>\n\n    <!--A funciton that takes in the most recent cards released and creates small boxes for each card containing img, name and price.-->\n    <div class=\"cardBox\">\n        <img class=\"cardImg\"></img>\n        <p class=\"cardName\"></p>\n        <p class=\"cardCost\"></p>\n    </div>\n";
-}
-=======
 export function renderBrowseCardPage () {
     return document.querySelector("#pageContent").innerHTML = `
     <header> 
@@ -30,4 +22,3 @@ export function renderBrowseCardPage () {
     </div>
 `;
 }
->>>>>>> fix/converting-code-to-js
--- src/home/home.js
+++ src/home/home.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.renderHomePage = renderHomePage;
-var recentlyViewedCard_ts_1 = require("./recentlyViewedCard.ts");
-var browseCard_ts_1 = require("../browse/browseCard.ts");
-function renderHomePage() {
-    document.querySelector("#pageContent").innerHTML = "\n        <div class=\"middleRow\">\n            <!--A button that sends the user to another html page for exp; cards that are hot on the market.-->\n            <button type=\"button\" id=\"browseCardBtn\">Browse Cards</button>\n\n            <!--A button that sends the user to another html page for their collections.-->\n            <button type=\"button\" class=\"myCollectionBtn\">My Collection</button>\n        </div>\n\n        <!--A slideshow that show the most recent cards the user has viewed, if not logged in it will show recently released sets.-->\n        <div id=\"recentlyViewedCards\">\n            <div class=\"slideshow-container\">\n\n            <div class=\"slides fade\">\n                <div class=\"numbertext\">1 / 3</div>\n                <img src=\"/mainProject/public/images/Naturbild1.jpg\">\n                <div class=\"text\">Card nr 1</div>\n            </div>\n\n            <div class=\"slides fade\">\n                <div class=\"numbertext\">2 / 3</div>\n                <img src=\"/mainProject/public/images/Naturbild1.jpg\">\n                <div class=\"text\">Card nr 2</div>\n            </div>\n\n            <div class=\"slides fade\">\n                <div class=\"numbertext\">3 / 3</div>\n                <img src=\"/mainProject/public/images/Naturbild1.jpg\">\n                <div class=\"text\">Card nr 3</div>\n            </div>\n\n            <a class=\"prev\">\u276E</a>\n            <a class=\"next\">\u276F</a>\n        </div>\n        <div style=\"text-align:center\">\n            <span class=\"dot\"></span> \n            <span class=\"dot\"></span> \n            <span class=\"dot\"></span> \n        </div>\n        </div>  \n    ";
-    var browseCardBtn = document.querySelector("#browseCardBtn");
-    console.log('browseCardBtn:', browseCardBtn);
-    if (browseCardBtn) {
-        console.log('Eventlistener has been added to browse!');
-        browseCardBtn === null || browseCardBtn === void 0 ? void 0 : browseCardBtn.addEventListener("click", function () {
-            console.log('Browse is being clicked!');
-            (0, browseCard_ts_1.renderBrowseCardPage)();
-        });
-    }
-    (0, recentlyViewedCard_ts_1.recentlyViewedCards)(document.querySelector('#recentlyViewedCards'));
-}
-=======
 import { recentlyViewedCards } from './recentlyViewedCard.js'
 import { renderBrowseCardPage } from '../browse/browseCard.js'
 
@@ -76,5 +56,3 @@ if (browseCardBtn) {
     });
 }
 }
-
->>>>>>> fix/converting-code-to-js
