d2fa04ef2190d8a27ea7c6bfb267c895c91ec649
Rasmus-Strångert <Rasmus_strangert@hotmail.com>
2025-09-10 10:45:12 +0200

    feat: Added so all button on main and home render their respective html injections when clicked. Also moved functions and imports in home.js to match the order they appear in the html.

--- src/browse/browseCard.js
+++ src/browse/browseCard.js
@@ -1,9 +1,9 @@
 export function renderBrowseCardPage () {
-    return document.querySelector("#pageContent").innerHTML = `
+    document.querySelector("#pageContent").innerHTML = `
     <!--A funciton that takes in the most recent cards released and creates small boxes for each card containing img, name and price.-->
     <div class="cardBox">
         <img class="cardImg"></img>
-        <p class="cardName"></p>
+        <p class="cardName">Use your imagination and visualise a bunch of cards here :)</p>
         <p class="cardCost"></p>
     </div>
 `;
